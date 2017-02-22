f49d1cf37472bc8111e20759ed072827f0b3092e
Nickforall <hello@nickforall.nl>
2017-02-22 10:54:06 +0100

    Basic quick prototype to get working on the clients

---
+++ index.js
@@ -0,0 +1,19 @@
+var io = require('socket.io')();
+
+console.log("Registering listeners...");
+io.on('connection', function(client){
+    console.log("Someone connected!");
+});
+
+const items =  ["#F44336", "#E91E63", "#9C27B0"];
+
+setInterval(function () {
+    var item = items[Math.floor(Math.random() * items.length)];
+    io.emit("newplayer", {
+        score: "0",
+        color: "#" + item
+    });
+}, 1000);
+
+console.log("Now listening on *:3000");
+io.listen(3000);
