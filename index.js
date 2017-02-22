6f128fc809dc9e9362ee6345354927c69cd67be9
Nickforall <hello@nickforall.nl>
2017-02-22 12:24:10 +0100

    Added randomized names to prototype

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ io.on('connection', function(client){
 });
 
 const items =  ["#F44336", "#E91E63", "#9C27B0"];
+const players = ["henk", "jan", "stan", "brandon", "nick", "geoffrey"];
 
 setInterval(function () {
     var item = items[Math.floor(Math.random() * items.length)];
+    var name = players[Math.floor(Math.random() * players.length)];
+
     io.emit("newplayer", {
         score: "0",
         color: item,
-	name: "Henk",
+	    name: name,
     });
 }, 1000);
 
