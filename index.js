32fa63e9827a0771ef0ae0b7fd0a40c9c1d9c739
Nickforall <hello@nickforall.nl>
2017-03-06 10:34:23 +0100

    More random names and prettier colors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var io = require('socket.io')();
+const io = require('socket.io')();
+const generateName = require('sillyname');
 
 console.log("Registering listeners...");
 io.on('connection', function(client){
@@ -8,18 +9,24 @@ io.on('connection', function(client){
 	});
 });
 
-const items =  ["#F44336", "#E91E63", "#9C27B0"];
-const players = ["henk", "jan", "stan", "brandon", "nick", "geoffrey"];
+const items =  ["#F44336", "#E91E63", "#9C27B0", "#673AB7",
+    "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4", "#009688",
+    "#4CAF50", "#8BC34A", "#FFEB3B", "#FFC107", "#FF5722"];
+
+var i = 0;
 
 setInterval(function () {
-    var item = items[Math.floor(Math.random() * items.length)];
-    var name = players[Math.floor(Math.random() * players.length)];
+    var item = items[i % items.length];
 
     io.emit("newplayer", {
         score: "0",
         color: item,
-	    name: name,
+	    name: generateName(),
     });
+
+    console.log(item);
+
+    i++;
 }, 1000);
 
 console.log("Now listening on *:3000");
