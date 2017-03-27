b19c47ba9c9ea8f80aa8136ce893601eb02446ed
Nickforall <hello@nickforall.nl>
2017-03-27 16:09:51 +0200

    Production data broadcasting

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const io = require('socket.io')();
 const generateName = require('sillyname');
 
+function calculateDistance(obj1, obj2) {
+    let a = Math.pow((obj1.x * obj2.x), 2);
+    let b = Math.pow((obj1.y * obj2.y), 2);
+
+    return Math.sqrt(a + b)
+}
+
+var players = [];
+
+const items =  ["#F44336", "#E91E63", "#9C27B0", "#673AB7",
+    "#3F51B5", "#03A9F4", "#00BCD4", "#009688", "#4CAF50",
+    "#8BC34A", "#FFEB3B", "#FFC107", "#FF5722"];
+    var item = items[i % items.length];
+
+var i = 0;
+
+function createPlayer(coord) {
+    i++;
+
+    let p = {
+        id: i,
+        score: "0",
+        color: item,
+	    name: generateName(),
+        coord: coord
+    }
+
+    players.push(p);
+}
+
 console.log("Registering listeners...");
-io.on('connection', function(client){
+io.on('connection', client => {
     console.log("Someone connected!");
 
-    client.on("playerid", function(data) {
+    client.on("playerid", data => {
         let id = data.playerNumber;
 
         setTimeout(function () {
@@ -15,18 +45,71 @@ io.on('connection', function(client){
         }, 10000 + Math.floor(Math.random() * 15000));
     });
 
-	client.on("scorebord", function(data) {
-		console.log(data)
-	});
+    client.on("objectupdate", data => {
+        if(data.length == 0) {
+            players = [];
+            return;
+        };
+
+        console.log(JSON.stringify(players));
+
+
+        var firstIteration = false;
+
+        if(players.length == 0) {
+            firstIteration = true;
+        }
+
+        for (var j = 0; j < data.length; j++) {
+            let coord = data[j];
+
+            if(firstIteration) {
+                createPlayer(coord);
+                console.log("Created Player!")
+                continue;
+            }
+
+            while(players.length > data.length) {
+                let furthestawaydis = -1;
+                let highestp = 0;
+
+                for (var l = 0; l < players.length; l++) {
+                    let p = players[l];
+                    let dis = calculateDistance(coord, p.coord)
+
+                    if(dis > 30) {
+                        furthestawaydis = dis;
+                        highestp = l
+                        break;
+                    }
+                }
+
+                players.splice(highestp, 1);
+            }
+
+            var distances = [];
+
+            for (var k = 0; k < players.length; k++) {
+                let p = players[k];
+                let dis = calculateDistance(coord, p.coord);
+                distances.push(Math.floor(dis))
+            }
+
+            let highest = distances.indexOf(Math.min.apply(null, distances))
+            players[highest].coord = coord;
+
+            let player = players[highest];
+            io.emit("playermove", [{
+                id: player.id,
+                color: player.color,
+                x: player.coord.x,
+                y: player.coord.y
+            }]);
+        }
+    });
 });
 
-const items =  ["#F44336", "#E91E63", "#9C27B0", "#673AB7",
-    "#3F51B5", "#03A9F4", "#00BCD4", "#009688", "#4CAF50",
-    "#8BC34A", "#FFEB3B", "#FFC107", "#FF5722"];
-
-var i = 0;
-
-/* SCOREBOARD DEMODATA */
+/* SCOREBOARD DEMODATA
 setInterval(function () {
     var item = items[i % items.length];
 
@@ -53,7 +136,7 @@ function getRandomColor() {
     return color;
 }
 
-/* PROJECTIE DEMO DATA */
+/* PROJECTIE DEMO DATA
 setInterval(function() {
     var boolX = Math.random() >= 0.4;
     var boolY = Math.random() >= 0.4;
@@ -69,7 +152,7 @@ setInterval(function() {
         x = 0; y = 0; ++playerId;
         color = items[i % items.length];
     }
-}, 100);
+}, 100);*/
 
 console.log("Now listening on *:3000");
 io.listen(3000);
